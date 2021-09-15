c3099c9c42a105b7fab3bbe3ecbd33de02099837
j3kaiii <j3kaiii@gmail.com>
2021-09-15 18:20:39 +0300

    Создали контроллеры для входящих и исходящих потоков. Для удобства создали гетеры

--- lib/color_bloc.dart
+++ lib/color_bloc.dart
@@ -1,7 +1,19 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 enum ColorEvent { event_red, event_green }
 
 class ColorBloc {
   Color _color = Colors.red;
+
+  // создали контроллер для входящих событий
+  final _inputEvenController = StreamController<ColorEvent>();
+  // гетер для контроллера. Через sink(входной поток) контроллер получает события
+  StreamSink<ColorEvent> get inputEventSink => _inputEvenController.sink;
+
+  // создали контроллер для исходящих данных
+  final _outputStateController = StreamController<Color>();
+  // геттер, через stream(исходящий поток) передает данные наружу
+  Stream<Color> get outputStateStream => _outputStateController.stream;
 }

72aa4400a767645129fec9f2b3c32c7af4fd49be
j3kaiii <j3kaiii@gmail.com>
2021-09-15 18:58:46 +0300

    Добавлен метод dispose()

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:bloc_with_streams/color_bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// ignore: use_key_in_widget_constructors
 class BlocPage extends StatefulWidget {
   @override
   _BlocPageState createState() => _BlocPageState();
@@ -27,10 +28,16 @@ class _BlocPageState extends State<BlocPage> {
   final ColorBloc _bloc = ColorBloc();
 
   @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bloc App'),
+        title: const Text('Bloc App'),
         centerTitle: true,
       ),
       body: Center(
@@ -46,7 +53,7 @@ class _BlocPageState extends State<BlocPage> {
                 height: 100,
                 width: 100,
                 color: snapshot.data,
-                duration: Duration(milliseconds: 500),
+                duration: const Duration(milliseconds: 500),
               );
             }),
       ),
@@ -60,7 +67,7 @@ class _BlocPageState extends State<BlocPage> {
             },
             backgroundColor: Colors.red,
           ),
-          SizedBox(width: 10),
+          const SizedBox(width: 10),
           FloatingActionButton(
             onPressed: () {
               // во входящий поток bloc передаем событие
